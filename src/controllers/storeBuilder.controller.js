3b651ba0572bd46ee5f7cb6edc07e5eda612bf1b
sudha <sudha@tangotech.co.in>
2025-01-24 13:13:17 +0530

    update status

--- src/controllers/storeBuilder.controller.js
+++ src/controllers/storeBuilder.controller.js
@@ -413,62 +413,86 @@ export async function storeLayout( req, res ) {
       return res.sendError( 'No data found', 204 );
     }
     let fixtureDetails = await storeFixtureService.find( { planoId: { $in: idList } } );
+    let fixtureList = fixtureDetails.map( ( ele ) => ele._id );
+    let fixtureQuery = [
+      {
+        $match: {
+          fixtureId: { $in: fixtureList },
+        },
+      },
+      {
+        $group: {
+          _id: '$fixtureId',
+          count: { $sum: '$shelfCapacity' },
+          fixtureId: { $first: '$fixtureId' },
+        },
+      },
+      {
+        $lookup: {
+          from: 'planocompliances',
+          let: { 'id': '$fixtureId' },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { date: new Date( dayjs().format( 'YYYY-MM-DD' ) ) },
+                    { $eq: [ '$fixtureId', '$$id' ] },
+                  ],
+                },
+              },
+            },
+            {
+              $group: {
+                _id: '$fixtureId',
+                properCount: { $sum: {
+                  $cond: [
+                    {
+                      $and: [
+                        { $eq: [ '$compliance', 'proper' ] },
+                      ],
+                    }, 1, 0 ],
+                } },
+                misPlacedCount: { $sum: {
+                  $cond: [
+                    {
+                      $and: [
+                        { $ne: [ '$compliance', 'proper' ] },
+                      ],
+                    }, 1, 0 ],
+                } },
+              },
+            },
+          ],
+          as: 'planoCompliance',
+        },
+      },
+    ];
+    let fixtureStatus = await fixtureShelfService.aggregate( fixtureQuery );
     for ( let layout of getStoreList ) {
       for ( let floor of layout.floor ) {
         for ( let polygon of floor.layoutPolygon ) {
           let polygonfixtureDetails = fixtureDetails.filter( ( ele ) => layout.planoId.toString() == ele.planoId.toString() && ele.floorId.toString() == floor.id.toString() && ele.associatedElementType == polygon.elementType && ele.associatedElementNumber == polygon.elementNumber );
-          let fixtures = [];
-          for ( let fixture of polygonfixtureDetails ) {
-            let data = { ...fixture._doc, status: '' };
-            let query = [
-              {
-                $match: {
-                  fixtureId: fixture._id,
-                },
-              },
-              {
-                $group: {
-                  _id: '',
-                  count: { $sum: '$shelfCapacity' },
-                  fixtureId: { $first: '$fixtureId' },
-                },
-              },
-              {
-                $lookup: {
-                  from: 'planocompliances',
-                  let: { 'id': '$fixtureId' },
-                  pipeline: [
-                    {
-                      $match: {
-                        $expr: {
-                          $and: [
-                            { date: new Date( dayjs().format( 'YYYY-MM-DD' ) ) },
-                            { $eq: [ '$fixtureId', '$$id' ] },
-                            { $eq: [ '$compliance', 'proper' ] },
-                          ],
-                        },
-                      },
-                    },
-                    {
-                      $group: {
-                        _id: '',
-                        count: { $sum: 1 },
-                      },
-                    },
-                  ],
-                  as: 'planoCompliance',
-                },
-              },
-            ];
-            let fixtureDetails = await fixtureShelfService.aggregate( query );
-            if ( fixtureDetails.length ) {
-              data.status = fixtureDetails[0]?.planoCompliance?.length ? fixtureDetails[0].count == fixtureDetails[0]?.planoCompliance?.count ? 'complete' : 'incomplete' : '';
+          polygonfixtureDetails.forEach( ( fixtureEle, index ) => {
+            let updateFixture = { ...fixtureEle._doc, status: '' };
+            let getFixtureStatus = fixtureStatus.find( ( statusElem ) => statusElem.fixtureId.toString() == fixtureEle._id.toString() );
+            if ( getFixtureStatus && getFixtureStatus?.planoCompliance?.length ) {
+              updateFixture.status = getFixtureStatus.count == getFixtureStatus?.planoCompliance?.properCount ? 'complete' : 'incomplete';
             }
-            fixtures.push( data );
-          }
-          polygon.fixture = fixtures;
+            polygonfixtureDetails[index] = updateFixture;
+          } );
+
+          polygon.fixture = polygonfixtureDetails;
         }
         floor.centerFixture = fixtureDetails.filter( ( ele ) => layout.planoId.toString() == ele.planoId.toString() && ele.floorId.toString() == floor.id.toString() && ele.fixtureType == 'floor' );
+        floor.centerFixture.forEach( ( centerFixture, index ) => {
+          let centerList = { ...centerFixture._doc, status: '' };
+          let getFixtureStatus = fixtureStatus.find( ( statusElem ) => statusElem.fixtureId.toString() == centerFixture._id.toString() );
+          if ( getFixtureStatus && getFixtureStatus?.planoCompliance?.length ) {
+            centerList.status = getFixtureStatus.count == getFixtureStatus?.planoCompliance?.properCount ? 'complete' : 'incomplete';
+          }
+          floor.centerFixture[index] = centerList;
+        } );
       }
     }
     return res.sendSuccess( { FloorDetails: getStoreList, productResolutionLevel: getPlanoDetails?.productResolutionLevel || '', productResolutionFilters: getPlanoDetails?.productResolutionFilters || [] } );
@@ -705,7 +729,6 @@ export async function scan( req, res ) {
         break;
     }
     query = { ...query, rfId: req.body.rfId };
-    console.log( query );
 
     let planoProductDetails = await planoMappingService.findOne( query );
     let data = {
