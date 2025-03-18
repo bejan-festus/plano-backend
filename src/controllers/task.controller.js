79c7be12800ad99866c0130ba1648d569c837cca
sudha <sudha@tangotech.co.in>
2025-03-18 10:59:55 +0530

    fixed issue

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -471,7 +471,7 @@ export async function updateAnswers( req, res ) {
       type: req.body.type,
     };
 
-    await planoTaskService.updateOne( { fixtureId: req.body.fixtureId, type: req.body.type }, data );
+    await planoTaskService.updateOne( { planoId: req.body.planoId, floorId: req.body.floorId, fixtureId: req.body.fixtureId, type: req.body.type, date_string: dayjs().format( 'YYYY-MM-DD' ) }, data );
     return res.sendSuccess( 'Fixture details updated successfully' );
   } catch ( e ) {
     logger.error( { functionName: 'updateAnswers', error: e } );
