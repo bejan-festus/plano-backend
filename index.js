2735b05785b1c611a9dbfb7c52cfab4473fab60c
bejan festus <bejan@tangotech.co.in>
2025-04-28 13:21:08 +0530

    api changes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { storeBuilderRouter } from './src/routes/storeBuilder.routes.js';
 import { storeBuilderTaskRouter } from './src/routes/task.routes.js';
 import { scriptRouter } from './src/routes/script.routes.js';
+import { planoLibraryRouter } from './src/routes/planoLibrary.routes.js';
+import { fixtureTemplateRouter } from './src/routes/fixtureTemplate.routes.js';
+import { managePlanogramRouter } from './src/routes/managePlanogram.routes.js';
 
 
-export { storeBuilderRouter, storeBuilderTaskRouter, scriptRouter };
+export { storeBuilderRouter, storeBuilderTaskRouter, scriptRouter, planoLibraryRouter, fixtureTemplateRouter, managePlanogramRouter };
