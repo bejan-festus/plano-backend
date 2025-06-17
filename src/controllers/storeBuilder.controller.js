15132743a455ce0f4a293d612a9fba1e6ce389bb
Bejan Festus D <bejan@tangotech.co.in>
2025-06-17 15:53:23 +0530

    api changes

--- src/controllers/storeBuilder.controller.js
+++ src/controllers/storeBuilder.controller.js
@@ -3124,7 +3124,6 @@ export async function storeFixturesTaskv2( req, res ) {
                       const vmDetails = await Promise.all( fixture.toObject()?.vmConfig?.map( async ( vm ) => {
                         totalVmCount += 1;
                         const vmInfo = await planoVmService.findOne( { _id: vm.vmId } );
-                        console.log(vmInfo?.toObject())
                         return {
                           ...vm,
                           ...vmInfo?.toObject(),
