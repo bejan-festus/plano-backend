25511f37bbc7bae89758339cc033cbe9bc0c93be
Sudha Github <sudha@tangotech.co.in>
2025-07-02 17:44:31 +0530

    fixed issue

--- src/controllers/storeBuilder.controller.js
+++ src/controllers/storeBuilder.controller.js
@@ -3368,19 +3368,6 @@ export async function planoList( req, res ) {
             },
             { $sort: { _id: -1 } },
             {
-              $group: {
-                _id: { floorId: '$floorId', type: '$type' },
-                doc: { $first: '$$ROOT' },
-              },
-            },
-            {
-              $project: {
-                type: '$_id.type',
-                floorId: '$_id.floorId',
-                answers: '$doc.answers',
-              },
-            },
-            {
               $set: {
                 hasPendingIssues: {
                   $anyElementTrue: {
@@ -3952,7 +3939,6 @@ export async function planoList( req, res ) {
     ];
 
     let pendingDetails = await planotaskService.aggregate( taskQuery );
-
     taskQuery = [
       {
         $match: {
@@ -4074,6 +4060,7 @@ export async function planoList( req, res ) {
     ];
 
     pendingDetails = await planoTaskComplianceService.aggregate( taskQuery );
+    // console.log( JSON.stringify( pendingDetails ) );
     let result = {
       data: planoDetails[0].data,
       count: planoDetails?.[0]?.count?.[0]?.total || 0,
